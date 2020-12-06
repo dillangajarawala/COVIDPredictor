0c600de5b6252d792bd7fdeed27e72232ade147a
Dillan Gajarawala <dillan.gajarawala@gmail.com>
2020-12-06 18:22:17 -0500

    skeleton api to be updated with parameter checks and calculation

--- api/api.py
+++ api/api.py
@@ -1,11 +1,25 @@
-import flask
+from flask import Flask, request, redirect, render_template
+from cases_predictor import CasesPredictor
+from deaths_predictor import DeathsPredictor
 
-app = flask.Flask(__name__)
+app = Flask(__name__)
 app.config["DEBUG"] = True
+
+cases_predictor = CasesPredictor()
+deaths_predictor = DeathsPredictor()
 
 
 @app.route('/', methods=['GET'])
 def home():
-    return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
+    return render_template("home")
+
+@app.route('/predictcases', methods=["GET", "POST"])
+def predict_cases():
+    return render_template("predict_cases")
+
+@app.route('/predictdeaths', methods=["GET", "POST"])
+def predict_deaths():
+    return render_template("predict_deaths")
+
 
 app.run()
